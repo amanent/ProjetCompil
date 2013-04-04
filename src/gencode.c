31c2d9af47bee78896a96275c1c34c93920de411
deneb <contact@bblois.fr>
2013-04-04 11:20:18 +0200

    added IF code generation, and all non generating code flag

--- src/gencode.c
+++ src/gencode.c
@@ -44,16 +44,18 @@ string strcatwalloc(string s1, string s2) {
 	return res;
 }
 
+/* variables annexes nécéssaires, je vais les replacer plus loin */
+int nbIf=0;
+/*-----------------------------------------------*/
+
 string gencode(TreeP tree) {
 	char *tmp=NULL;
-	char intToStr[20] = ""; /* normalement pas de int de plus de 20 digit gérés par la machine (18 max pour un nombre sur 64 bits, plus le signe plus \0 = 20) */
+	char intToStr[20] = "", intToStr2[20] = ""; /* normalement pas de int de plus de 20 digit gérés par la machine (18 max pour un nombre sur 64 bits, plus le signe plus \0 = 20) */
 
 	if (tree == NULL)
 	    return "";
 	//printf("treating : %d\n", tree->op);
 	switch (tree->op) {
-		case ID: break;
-		case IDCL: break; 
 		case STR: 
 			return writeCode(NULL, TRUE, NULL, "PUSHS", tree->u.str, "chaine de caractere"); 
 		case CST:
@@ -93,26 +95,31 @@ string gencode(TreeP tree) {
 		case CONCAT:
 			tmp = strcatwalloc(gencode(getChild(tree, 0)), gencode(getChild(tree, 1)));
 			return writeCode(tmp, TRUE, NULL, "CONCAT", NULL, "concatenation");
-		case BLCDECL: break; 	
-		case DECL: break;		
-		case LSTINST: 
-			return strcatwalloc(gencode(getChild(tree, 0)), gencode(getChild(tree, 1)));
-		case VAR: break; 		
-		case MSGSNT: break; 	
-		case CAST: break; 		
-		case INST:
-			return strcatwalloc(gencode(getChild(tree, 0)), gencode(getChild(tree, 1)));	
-		case SELECT: break;
 		case UNARYSUB:
 			tmp = writeCode(NULL, TRUE, NULL, "PUSHI", "0", "moins unaire...");
 			tmp = strcatwalloc(tmp, gencode(getChild(tree, 0)));
 			return writeCode(tmp, TRUE, NULL, "SUB", NULL, "... fin moins unaire");
 		case UNARYADD: 
 			return gencode(getChild(tree, 0));
-		case IF: break;
+		case IF: 
+			sprintf(intToStr, "else_%d", nbIf);
+			tmp = writeCode(gencode(getChild(tree, 0)), TRUE, NULL, "JZ", intToStr, "premiere passe if");
+			tmp = strcatwalloc(tmp, gencode(getChild(tree, 1)));
+			sprintf(intToStr2, "suite_%d", nbIf);
+			tmp = writeCode(tmp, TRUE, NULL, "JUMP", intToStr2, "deuxieme passe if (fin du then)");
+			tmp = writeCode(tmp, TRUE, intToStr, "NOP", NULL , "troisième passe du if (else)");
+			tmp = strcatwalloc(tmp, gencode(getChild(tree, 1)));
+			return writeCode(tmp, TRUE, intToStr2, "NOP", NULL , "quatrième passe du if (fin du else)");
 		case AFF: break;
 		case RET: break;
-		case LSTARG: break;
+		case VAR: break; 		
+		case MSGSNT: break; 	
+		case CAST: break; 		
+		case SELECT: break;
+		case ID: break;
+		case IDCL: break; 
+		case LSTARG: case BLCDECL: case DECL: case LSTINST: case INST: 
+			return strcatwalloc(gencode(getChild(tree, 0)), gencode(getChild(tree, 1)));
 		default:
 		fprintf(stderr, "Erreur! pprint : etiquette d'operator inconnue: %d\n", tree->op);
 		break; 
@@ -155,10 +162,6 @@ AFF
 	PUSH // valeur 
 	STORE // decalage du champ par rapport a la classe... comment est ce que je le retrouve moi ?) 
 RET
-LSTA
-BLCDECL
-DECL
-LSTINST
 VAR
 
 */
